1a9bc62cacd90596bff02c200d84c5ece27f540b
OOHYEE <azss0470@gmail.com>
2021-11-16 13:54:14 +0900

    Encapsulation 적용해보기

---
+++ 3-oop/3-3-encapsulation.ts
@@ -0,0 +1,43 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMachine {
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    // 생성자를 이용하여 오브젝트를 만드는 것을 금지한다.
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    // static method를 이용하여 오브젝트를 생성하도록 권장
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const coffeeMachine = CoffeeMachine.makeMachine(12);
+  coffeeMachine.fillCoffeeBeans(32);
+  console.log(coffeeMachine);
+}
