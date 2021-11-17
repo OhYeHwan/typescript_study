e58f7bd04bfe9d6cc1e88e8865d6d802a58c9472
OOHYEE <azss0470@gmail.com>
2021-11-17 14:06:20 +0900

    Stack 구현하기

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -32,4 +32,37 @@
       return value;
     }
   }
+
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
+  };
+
+  // size에 대한 이해 부족
+  class StackI implements Stack {
+    private _size: number = 0;
+    private head?: StackNode;
+    get size(): number {
+      return this._size;
+    }
+
+    push(value: string) {
+      const node: StackNode = {
+        value,
+        next: this.head,
+      };
+      this.head = node;
+      this._size++;
+    }
+
+    pop(): string {
+      if (this.head == null) {
+        throw new Error("Stack is Empty");
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
+  }
 }
